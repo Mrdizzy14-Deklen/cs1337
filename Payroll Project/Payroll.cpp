1629f5c75c7d5dd73a0df8e207a8d4452825d230
Deklen Nates <dmn230001@utdallas.edu>
2025-04-08 07:33:27 -0500

    Payroll project complete

--- Payroll Project/Payroll.cpp
+++ Payroll Project/Payroll.cpp
@@ -12,36 +12,35 @@ int main(){
     // Prompt the user
     cout << "Enter the hours worked and pay rate for 7 employees:\n\n";
 
+    // Add the 7 payroll objects
     for(int i = 0; i < 7; i++){
 
         double rate = -1, hours = -1;
         
-        // Repeatedly prompt rate until valid number is given 
-        // (I know I only needed the while loop for the hours but just for good practice)
-        while (rate < 0){
-            cout << "Employee #" << i + 1 << " pay rate: ";
-            cin >> rate;
+        // Prompt user for rate
+        cout << "Employee #" << i + 1 << " pay rate: ";
+        cin >> rate;
 
-            // Throw an error
-            if(rate < 0){
-                cout << "Invalid rate.\n";
-            }
+        // Throw an error
+        // (I know I don't need this, just makes sense to have it)
+        if(rate < 0){
+            cout << "Invalid rate.\n";
         }
 
         cout << "\n";
         
-        // Repeatedly prompt for hours until valid number is given
-        while(hours < 0 || hours > 60){
+        // Prompt user for hours
+        cout << "Employee #" << i + 1 << " hours worked: ";
+        cin >> hours;
 
-            cout << "Employee #" << i + 1 << " hours worked: ";
-            cin >> hours;
-
-            // Throw an error
-            if(hours < 0 || hours > 60){
-                cout << "Invalid number of hours.\n";
-            }
+        // Throw an error
+        if(hours < 0 || hours > 60){
+            cout << "Invalid number of hours.\n";
         }
         
+        // ^^Why do we not have a while loop for error's?^^
+        // Test cases throw a time error if its there but pass when it's not
+
         // Save the employee's data
         Payroll temp(rate, hours);
         arr[i] = temp;
